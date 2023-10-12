50ad87998b2732790c55dc54531a5da901fc6571
MarsIfeanyi <marschalice@gmail.com>
2023-10-12 20:49:19 +0100

    .

--- src/BridgeWatersNFTPlatform.sol
+++ src/BridgeWatersNFTPlatform.sol
@@ -3,22 +3,24 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "./BridgeWatersToken.sol";
 
-contract BridgeWatersNFTPlatform {
+contract BridgeWatersNFTPlatform is IERC721Receiver {
     using SafeMath for uint256;
-
-    address public owner;
-    uint256 public platformFee;
-    mapping(address => mapping(uint256 => NFTListing)) public nftListings;
-
     struct NFTListing {
         address owner;
-        address nftContract;
+        address nftAddress;
         uint256 tokenId;
-        ERC20 fractionalToken;
+        address FractionalToken;
         uint256 totalShares;
         uint256 pricePerShare;
     }
+
+    address public owner;
+    uint256 public platformFee;
+    mapping(address => NFTListing[]) private nFTListings;
+    mapping(address => mapping(uint256 => uint256)) public nftListingIndex;
 
     event NFTListed(
         address indexed owner,
@@ -46,41 +48,51 @@ contract BridgeWatersNFTPlatform {
     error InsufficientFundsSent();
     error InsufficientSharesToTransfer();
 
+    constructor() {
+        owner = msg.sender;
+    }
+
     function listNFT(
-        address _nftContract,
+        address _nftAddress,
         uint256 _tokenId,
         uint256 _totalShares,
         uint256 _pricePerShare
     ) external {
+        IERC721 nftContract = IERC721(_nftAddress);
+
         if (_totalShares < 0) revert TotalSharesMustBeGreaterThanZero();
 
         if (_pricePerShare < 0) revert PricePerShareMustBeGreaterThanZero();
 
-        NFTListing storage listing = nftListings[_nftContract][_tokenId];
+        nftListingIndex[_nftAddress][_tokenId] = nFTListings[msg.sender].length;
+
+        BridgeWatersToken fractionalToken = new BridgeWatersToken();
 
         if (listing.owner == address(0)) revert NFTIsAlreadyListed();
 
-        ERC721 nft = ERC721(_nftContract);
+        if (nftContract.ownerOf(_tokenId) != msg.sender) revert NotNFTOwner();
 
-        if (nft.ownerOf(_tokenId) != msg.sender) revert NotNFTOwner();
+        nFTListings[_nftAddress].push(
+            NFTListing(
+                msg.sender,
+                _nftAddress,
+                _tokenId,
+                _pricePerShare,
+                _totalShares
+            )
+        );
 
-        listing.owner = msg.sender;
-        listing.nftContract = _nftContract;
-        listing.tokenId = _tokenId;
-        listing.totalShares = _totalShares;
-        listing.pricePerShare = _pricePerShare;
+        nftContract.safeTransferFrom(msg.sender, address(this), _tokenId);
 
-        nft.safeTransferFrom(msg.sender, address(this), _tokenId);
-
-        emit NFTListed(msg.sender, _nftContract, _tokenId);
+        emit NFTListed(msg.sender, _nftAddress, _tokenId);
     }
 
     function purchaseNFT(
-        address _nftContract,
+        address _nftAddress,
         uint256 _tokenId,
         uint256 _sharesToPurchase
     ) external payable {
-        NFTListing storage listing = nftListings[_nftContract][_tokenId];
+        NFTListing storage listing = nftListingIndex[_nftAddress][_tokenId];
 
         if (listing.owner == address(0)) revert NFTListingDoesNotExist();
 
@@ -90,15 +102,19 @@ contract BridgeWatersNFTPlatform {
         if (msg.value <= _sharesToPurchase.mul(listing.pricePerShare))
             revert InsufficientFundsSent();
 
+        uint256 _nftListingIndex = nftListingIndex[_nftAddress][_tokenId];
+
+        BridgeWatersToken fractionalToken = new BridgeWatersToken();
+
         uint256 _platformFee = msg.value.mul(platformFee).div(1000);
         uint256 purchaseValue = msg.value.sub(platformFee);
 
         listing.totalShares = listing.totalShares.sub(_sharesToPurchase);
 
         // Transfer fractional tokens instead of the entire NFT
-        listing.fractionalToken.transfer(msg.sender, _sharesToPurchase);
+        fractionalToken.transfer(msg.sender, _sharesToPurchase);
 
-        emit NFTListed(msg.sender, _nftContract, _tokenId);
+        emit NFTListed(msg.sender, _nftAddress, _tokenId);
     }
 
     function transferNFT(
@@ -107,7 +123,7 @@ contract BridgeWatersNFTPlatform {
         address _to,
         uint256 _sharesToTransfer
     ) external {
-        NFTListing storage listing = nftListings[_nftContract][_tokenId];
+        NFTListing storage listing = nftListingIndex[_nftContract][_tokenId];
         if (listing.owner == address(0)) revert NFTListingDoesNotExist();
 
         listing.fractionalToken.transferFrom(
